149b5f04d161cf8dd1abb461493f3432ef91852c
Mubarak Mohideen N <n.mubarakmohideen@gmail.com>
2021-08-11 18:13:06 +0530

    product item design

---
+++ lib/presentation/ui/model/product.dart
@@ -0,0 +1,14 @@
+class Product {
+  final String image;
+  final String name;
+  final String storeImage;
+  final String storeName;
+  final String price;
+
+  Product(
+      {required this.image,
+      required this.name,
+      required this.storeImage,
+      required this.storeName,
+      required this.price});
+}
--- lib/presentation/widget/product_item.dart
+++ lib/presentation/widget/product_item.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:tradly_grocery_app/presentation/ui/model/product.dart';
 import 'package:tradly_grocery_app/presentation/utils/app_text_style.dart';
 
 class ProductItem extends StatelessWidget {
 
-  ProductItem();
+  final Product _product;
+
+  ProductItem({required Product product}): this._product = product;
 
   @override
   Widget build(BuildContext context) {
@@ -15,13 +18,29 @@ class ProductItem extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Image.network('assets/ic_profile_picture.png',
+          Image.network(this._product.image,
             width: 160.0,
             height: 130.0,
             fit: BoxFit.fill,),
           Padding(
             padding: EdgeInsets.only(left: 12.0, right: 6.0, top: 13.0), child
-            : Text('Coca Cola',style: getStyleSubTitle(context),),)
+            : Text(this._product.name,style: getStyleSubTitle(context),),),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Row(
+                children: [
+                  Image.network(this._product.storeImage,
+                    width: 30.0,
+                    height: 30.0,
+                    fit: BoxFit.fill,),
+                  Text(this._product.storeName)
+                ],
+              ),
+              Text(this._product.price)
+            ],
+          )
         ],
       ),
     );
