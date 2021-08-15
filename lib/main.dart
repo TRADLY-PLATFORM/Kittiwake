cda62e2f6f52e1c9b853ad01ef033a4ac45ebe14
Mubarak Mohideen N <n.mubarakmohideen@gmail.com>
2021-08-15 09:46:40 +0530

    Sentry SDK Integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 import 'package:tradly_grocery_app/locator.dart';
 import 'app.dart';
 
-void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-    // For Android.
-    // Use [light] for white status bar and [dark] for black status bar.
-    statusBarIconBrightness: Brightness.light,
-    // For iOS.
-    // Use [dark] for white status bar and [light] for black status bar.
-    statusBarBrightness: Brightness.dark,
-  ));
-  SystemChrome.setPreferredOrientations([
-    DeviceOrientation.portraitUp,
-  ]);
-  setUpRealLocator();
-  //setupMockLocator();
-  runApp(App());
+Future<void> main() async {
+  await SentryFlutter.init(
+        (options) {
+      options.dsn = 'https://b3598089ccc74b5887130e9bcb24fe62@o396771.ingest.sentry.io/5906572';
+    },
+    appRunner: () {
+      WidgetsFlutterBinding.ensureInitialized();
+      SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+        // For Android.
+        // Use [light] for white status bar and [dark] for black status bar.
+        statusBarIconBrightness: Brightness.light,
+        // For iOS.
+        // Use [dark] for white status bar and [light] for black status bar.
+        statusBarBrightness: Brightness.dark,
+      ));
+      SystemChrome.setPreferredOrientations([
+        DeviceOrientation.portraitUp,
+      ]);
+      setUpRealLocator();
+      //setupMockLocator();
+      runApp(App());
+      }
+  );
 }
