fc64442b6eaaf44ffa19fe3b45c14fc595ce775e
Mubarak Mohideen N <n.mubarakmohideen@gmail.com>
2021-09-21 00:53:37 +0530

    login unit test updated

--- test/unit_test.dart
+++ test/unit_test.dart
@@ -1,18 +1,19 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:tradly_grocery_app/domain/model/params_login.dart';
-import 'package:tradly_grocery_app/domain/usecase/login_use_case.dart';
+import 'package:tradly_grocery_app/domain/usecase/use_case.dart';
 import 'package:tradly_grocery_app/locator.dart';
 
-void main(){
+void main() {
   setUpRealLocator();
 
   group('Tradly Grocery Flow', () {
     test('Login SHOULD Pass THEN RETURN true', () async {
-      final loginUseCase = locator<LoginUseCase>();
-      final paramsLogin = ParamsLogin(uuid: '', email: 'mthahaseen@gmail.com', password: '12345678');
+      final loginUseCase = locator<UseCase<bool, ParamsLogin>>();
+      final paramsLogin = ParamsLogin(
+          uuid: '', email: 'mthahaseen@gmail.com', password: '12345678');
       final loginResponse = await loginUseCase.execute(paramsLogin);
-      expect(loginResponse.isRight, true);
-      expect(loginResponse.right, true);
+      expect(loginResponse.isLeft, true);
+      expect(loginResponse.left, true);
     });
   });
 }
